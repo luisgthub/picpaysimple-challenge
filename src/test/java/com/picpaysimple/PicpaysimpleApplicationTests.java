3383ba38dd0e69af901b7b7604f6b49f7b1c178d
luisgthub <luiamaral2@gmail.com>
2023-12-02 02:06:44 -0300

    feat: initial config

---
+++ src/test/java/com/picpaysimple/PicpaysimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpaysimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaysimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
